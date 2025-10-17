5068a0383a66c488daaa0004251aa01eaa2cb823
Angel Valladares <arivalladares2.0@gmail.com>
2025-10-17 03:07:24 -0600

    Update page title in metadata for better clarity and SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Yiddish Jobs - Find Your Perfect Yiddish Job",
+  title: "Yid Jobs in Boro Park | Yiddish Jobs",
   description: "Discover thousands of yiddish opportunities and yid job positions from our extensive professional database. Connect with ambitious career opportunities today.",
   icons: {
     icon: "/favico.png",
