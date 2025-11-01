1a328c64bcb4b25da255c2df9248d9250b72ebc5
Angel Valladares <arivalladares2.0@gmail.com>
2025-11-01 13:18:36 -0600

    feat: Add viewport meta tag for responsive design in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -142,6 +142,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
