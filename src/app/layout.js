95465af2bf36e141e9f3385688b82c1b11fa53b1
Angel Valladares <arivalladares2.0@gmail.com>
2025-12-28 11:17:44 -0600

    microsoft clarity integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -161,6 +162,15 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={inter.className}>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r8xf71hs7t");
+          `}
+        </Script>
         <Header />
         <main>{children}</main>
         <Footer />
